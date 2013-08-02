1444d4dc49656f8534a3254928644e95f9b3c086
lwenke <lwenke@hotmail.com>
2013-08-02 12:09:14 +1000

    Likert Agree/Disagree now saves within survey section
    If you click 'Next' or 'Submit' and some required items need to be finished in the section then it will save the Likert Agree/Disagree items properly.

--- pages/site/questions/likert.php
+++ pages/site/questions/likert.php
@@ -5,11 +5,15 @@ if ($question['likertscale'] != "Agree/Disagree") {
 		<span class="wpsqt_likert_answer"><input type="radio" name="answers[<?php echo $questionKey; ?>]" value="<?php echo $i; ?>" <?php if ( $i == $givenAnswer ) { ?> checked="checked" <?php } ?> id="answer_<?php echo $question['id']; ?>_<?php echo $i; ?>" /> <label for="answer_<?php echo $question['id']; ?>_<?php echo $i; ?>"><?php echo $i; ?></label></span>
 	<?php }
 } else {
+	$likert_agree_disagree =  array( 'stronglydisagree' => 'Strongly Disagree',
+					'disagree' => 'Disagree',
+					'noopinion' => 'No Opinion',
+					'agree' => 'Agree',
+					'stronglyagree' => 'Strongly Agree');
+	
+	foreach ($likert_agree_disagree as $key => $answer_text):
 	?>
-		<span class="wpsqt_likert_answer"><input type="radio" name="answers[<?php echo $questionKey; ?>]" value="<?php _e('Strongly Disagree', 'wp-survey-and-quiz-tool'); ?>" id="answer_<?php echo $question['id']; ?>_stronglydisagree" /> <label for="answer_<?php echo $question['id']; ?>_stronglydisagree"><?php _e('Strongly Disagree', 'wp-survey-and-quiz-tool'); ?></label></span>
-	<span class="wpsqt_likert_answer"><input type="radio" name="answers[<?php echo $questionKey; ?>]" value="<?php _e('Disagree', 'wp-survey-and-quiz-tool'); ?>" id="answer_<?php echo $question['id']; ?>_disagree" /> <label for="answer_<?php echo $question['id']; ?>_disagree"><?php _e('Disagree', 'wp-survey-and-quiz-tool'); ?></label></span>
-<span class="wpsqt_likert_answer"><input type="radio" name="answers[<?php echo $questionKey; ?>]" value="<?php _e('No Opinion', 'wp-survey-and-quiz-tool'); ?>" id="answer_<?php echo $question['id']; ?>_noopinion" /> <label for="answer_<?php echo $question['id']; ?>_noopinion"><?php _e('No Opinion', 'wp-survey-and-quiz-tool'); ?></label></span>
-<span class="wpsqt_likert_answer"><input type="radio" name="answers[<?php echo $questionKey; ?>]" value="<?php _e('Agree', 'wp-survey-and-quiz-tool'); ?>" id="answer_<?php echo $question['id']; ?>_agree" /> <label for="answer_<?php echo $question['id']; ?>_agree"><?php _e('Agree', 'wp-survey-and-quiz-tool'); ?></label></span>
-<span class="wpsqt_likert_answer"><input type="radio" name="answers[<?php echo $questionKey; ?>]" value="<?php _e('Strongly Agree', 'wp-survey-and-quiz-tool'); ?>" id="answer_<?php echo $question['id']; ?>_stronglyagree" /> <label for="answer_<?php echo $question['id']; ?>_stronglyagree"><?php _e('Strongly Agree', 'wp-survey-and-quiz-tool'); ?></label></span>
+		<span class="wpsqt_likert_answer"><input type="radio" <?php checked( $givenAnswer, $answer_text );?> name="answers[<?php echo $questionKey; ?>]" value="<?php _e($answer_text, 'wp-survey-and-quiz-tool'); ?>" id="answer_<?php echo $question['id']; ?>_<?php echo $key;?>" /> <label for="answer_<?php echo $question['id']; ?>_<?php echo $key;?>"><?php _e($answer_text, 'wp-survey-and-quiz-tool'); ?></label></span>
+	<?php endforeach;?>
 <?php } ?>
 <br /><br />
